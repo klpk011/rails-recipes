22b68d1325fd94496a3cfc6ed0af52a5b0dec0f6
klpk011 <sophieQ011@gmail.com>
2017-05-09 17:13:26 +0800

    implement tickets function

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,12 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :events
-
+  
   namespace :admin do
     root "events#index"
-    resources :events
+    resources :events do
+      resources :tickets, :controller => "event_tickets"
+    end
     resources :users do
       resource :profile, :controller => "user_profiles"
     end
